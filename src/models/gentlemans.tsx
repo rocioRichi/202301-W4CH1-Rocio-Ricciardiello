5d275edf79b013d3716c25364771754e94bdf232
rocioRichi <paragit@gmail.com>
2023-02-06 20:39:45 +0100

    Create Gentleman class y mock file

---
+++ src/models/gentlemans.tsx
@@ -0,0 +1,12 @@
+export class Gentleman {
+  constructor(
+    public id: number,
+    public name: string,
+    public status: string,
+    public profession: string,
+    public twitter: string,
+    public picture: string,
+    public alternativeText: string,
+    public selected: boolean
+  ) {}
+}
